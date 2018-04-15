P79N6A/clinia-design

commit 76d3e8f6c61d3bb5c7012ee316bd740825147a2d

Use componentDidMount to replace componentWillMount: componentWillMount is deprecating (#10047)

packages/clinia-design-uikit/components/locale-provider/index.tsx
@@ -53,7 +53,7 @@ export default class LocaleProvider extends React.Component<LocaleProviderProps,
     };
   }
 
-  componentWillMount() {
+  componentDidMount() {
     setMomentLocale(this.props.locale);
     this.componentDidUpdate();
   }

commit 40a2e62d8ddf56c92649297e09ebd337c0e62b82

Fix test case of LocaleProvider
which is broken by #10047

use constructor over componentDidMount

packages/clinia-design-uikit/components/locale-provider/index.tsx
@@ -44,6 +44,12 @@ export default class LocaleProvider extends React.Component<LocaleProviderProps,
     antLocale: PropTypes.object,
   };
 
+  constructor(props: LocaleProviderProps) {
+    super(props);
+    setMomentLocale(props.locale);
+    changeConfirmLocale(props.locale && props.locale.Modal);
+  }
+
   getChildContext() {
     return {
       antLocale: {
@@ -51,11 +57,6 @@ export default class LocaleProvider extends React.Component<LocaleProviderProps,
         exist: true,
       },
     };
-  }
-
-  componentDidMount() {
-    setMomentLocale(this.props.locale);
-    this.componentDidUpdate();
   }
 
   componentWillReceiveProps(nextProps: LocaleProviderProps) {